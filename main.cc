beb88650e4d4facac616471e004f9520e8115610
Neo Esil <nguyendanganhthi247@gmail.com>
2018-05-24 14:02:41 +0700

    mask password with *

--- main.cc
+++ main.cc
@@ -1,6 +1,11 @@
 #include "libs/FTPClient.h"
 #include <string>
 #include <iostream>
+#include <termios.h>
+#include <unistd.h>
+
+int getch();
+std::string getpass(const char *prompt, bool show_asterisk = true);
 
 int main(int argc, char*argv[]) {
   if (argc < 2) {
@@ -8,13 +13,13 @@ int main(int argc, char*argv[]) {
     std::cout << "Usage: ftp <host>" << std::endl;
     exit(EXIT_FAILURE);
   }
+
   FTPClient ftpClient(argv[1]);
   std::string userName, password;
   std::cout << "Username: ";
   std::getline(std::cin, userName); 
   ftpClient.sendUsername(userName);
-  std::cout << "Password: ";
-  std::getline(std::cin, password);
+  password = getpass("Password: ", true); 
   bool resp = ftpClient.sendPassword(password);
   if (resp == false) {
     std::cout << "Login failed.\n";
@@ -33,4 +38,53 @@ int main(int argc, char*argv[]) {
     ftpClient.sendCommand(cmd);
   }
   return 0;
+}
+
+// Masking password input: http://www.cplusplus.com/articles/E6vU7k9E/
+int getch() {
+  struct termios oldt, newt;
+  int ch;
+  tcgetattr(STDIN_FILENO, &oldt);
+  newt = oldt;
+  newt.c_lflag &= ~(ICANON | ECHO);
+  tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+  ch = getchar();
+  tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+  return ch;
+}
+
+
+std::string getpass(const char *prompt, bool show_asterisk)
+{
+  using std::cout;
+  using std::endl;
+  using std::string;
+  const char BACKSPACE=127;
+  const char RETURN=10;
+
+  string password;
+  unsigned char ch=0;
+
+  cout <<prompt;
+
+  while((ch=getch())!=RETURN)
+    {
+       if(ch==BACKSPACE)
+         {
+            if(password.length()!=0)
+              {
+                 if(show_asterisk)
+                 cout <<"\b \b";
+                 password.resize(password.length()-1);
+              }
+         }
+       else
+         {
+             password+=ch;
+             if(show_asterisk)
+                 cout <<'*';
+         }
+    }
+  cout <<endl;
+  return password;
 }
